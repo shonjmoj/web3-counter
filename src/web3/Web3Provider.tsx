79baec694e9fc34b58c8a541495b4e1f360c5599
shonjmoj <midoounejjart@gmail.com>
2024-05-12 20:52:37 +0100

    chore : clean up

--- src/web3/Web3Provider.tsx
+++ src/web3/Web3Provider.tsx
@@ -7,17 +7,14 @@ import { createWeb3Modal } from "@web3modal/wagmi/react";
 
 import { State, WagmiProvider } from "wagmi";
 
-// Setup queryClient
-
 if (!projectId) throw new Error("Project ID is not defined");
 
-// Create modal
 createWeb3Modal({
   wagmiConfig: config,
   themeMode: "dark",
   projectId,
-  // enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  // enableOnramp: true, // Optional - false as defaul
+  enableAnalytics: true,
+  enableOnramp: true,
 });
 
 export default function Web3ModalProvider({
